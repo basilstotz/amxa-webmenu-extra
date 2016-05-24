47496a79c1a34a21aa561dbd6b63ad214823369c
root <root@bootserver.amxa.opinys.fi>
2016-05-24 13:52:16 +0200

    *

--- amxa-webmenu-extra-1.0/etc/profile.d/set-menu-env.sh
+++ amxa-webmenu-extra-1.0/etc/profile.d/set-menu-env.sh
@@ -1,21 +1,40 @@
 ORG=$(cat /etc/puavo/domain|cut -d. -f1)
 
+if test -n "$PUAVO_SESSION_PATH" -a -e "$PUAVO_SESSION_PATH"; then
+  TYPE=$(jq .user.user_type $PUAVO_SESSION_PATH|sed 's/"//g')
+else
+  TYPE="guest"
+fi
+
 case $ORG in
   anwil)
       LIST="M_PROP M_GAMES"
+      FIRST=""
       ;;
   basel)
       LIST="M_PROP M_WEBMENU_EDITOR M_GAMES"
+      FIRST="2014A 2014B 2015A 2015B"
       ;;
   ksnuwi)
       LIST="M_PROP M_GAMES"
+      FIRST=""
       ;;
   laeufelfingen)
       LIST="M_PROP M_GAMES"
+      FIRST=""
       ;;
   *)
       ;;
 esac
+
+if test "$TYPE" = "student"; then
+   GROUPS=$(groups)
+   for F in $FIRST; do
+      if echo $GROUPS|grep -q $F; then
+         export M_FIRST=1
+      fi
+   done
+fi
 
 for L in $LIST; do
   export $L=1
---
+++ amxa-webmenu-extra_1.0-1ubuntu36.dsc
@@ -0,0 +1,17 @@
+Format: 1.0
+Source: amxa-webmenu-extra
+Binary: amxa-webmenu-extra
+Architecture: all
+Version: 1.0-1ubuntu36
+Maintainer: Basil Stotz <stotz@amxa.ch>
+Homepage: <insert the upstream URL, if relevant>
+Standards-Version: 3.9.2
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ amxa-webmenu-extra deb unknown extra
+Checksums-Sha1: 
+ a12e26c4fba711327495e2bb6bd2a33540ff9052 26881 amxa-webmenu-extra_1.0-1ubuntu36.tar.gz
+Checksums-Sha256: 
+ 0b2b8f11017f8df365936e0f57456dc12d6be522c9df00177c2e4c47c6812078 26881 amxa-webmenu-extra_1.0-1ubuntu36.tar.gz
+Files: 
+ 0bfad8ab3c1eef0615f4b7796f2e1e62 26881 amxa-webmenu-extra_1.0-1ubuntu36.tar.gz
