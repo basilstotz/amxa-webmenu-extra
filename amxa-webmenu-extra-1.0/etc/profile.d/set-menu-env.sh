e73c613c85497c4855e56fbdece66775371d942f
root <root@bootserver.amxa.opinys.fi>
2016-06-03 09:44:22 +0200

    added class menus

--- amxa-webmenu-extra-1.0/etc/profile.d/set-menu-env.sh
+++ amxa-webmenu-extra-1.0/etc/profile.d/set-menu-env.sh
@@ -1,33 +1,53 @@
+#devicetype
+DEVICETYPE=$(jq .type /etc/puavo/device.json|sed 's/"//g')
+export PUAVO_DEVICETYPE="$DEVICETYPE"
+
 #organisation
 ORG=$(cat /etc/puavo/domain|cut -d. -f1)
-export PUAVO_ORG=$ORG
+export PUAVO_ORG="$ORG"
 
 #usertype
 if test -n "$PUAVO_SESSION_PATH" -a -e "$PUAVO_SESSION_PATH"; then
   TYPE=$(jq .user.user_type $PUAVO_SESSION_PATH|sed 's/"//g')
 else
-  TYPE="guest"
+  USERTYPE="guest"
 fi
-export PUAVO_USERTYPE=$TYPE
+export PUAVO_USERTYPE=$USERTYPE
 
 #school
 SCHOOL=$(ls -l /home| grep $(ls /home|head -n1)|xargs|cut -d\  -f4)
-export PUAVO_SCHOOL=$SCHOOL
+export PUAVO_SCHOOL="$SCHOOL"
 
 #class
 GROUPS=$(groups)
-if test "$TYPE" = "student"; then
+if test "$USERTYPE" = "student"; then
   for G in $GOUPS; do
     if echo $G|grep -q "20"; then
-      CLASS=$G
+      CLASS="$G"
     fi
   done
 else
-  CLASS=$TYPE
+  CLASS=$USERTYPE
 fi
-if ! test "$CLASS" = "$TYPE";  then
-  export PUAVO_CLASS=$CLASS
+if ! test "$CLASS" = "$USERTYPE";  then
+  export PUAVO_CLASS="$CLASS"
 fi
+
+export PUAVO_GROUPS=$(echo $GROUPS|sed 's/ /:/g')
+
+
+if test -e /etc/webmenu/menu_$CLASS.json; then
+    cp /etc/webmenu/menu_$CLASS.json $HOME/.config/webmenu/menu.json
+fi
+
+if test -e $HOME/.config/webmenu/menu.json; then
+  export M_CUSTOM=1
+fi
+
+if test -e /etc/webmenu/tabd_$CLASS.json; then
+    cp /etc/webmenu/tabd_$CLASS.json $HOME/.config/webmenu/tab.d/aaa-menu.json
+fi
+
 
 #like this (maybe)
 #wget -O $HOME/.config/webmenu/menu.json  \ 
@@ -36,42 +56,42 @@ fi
 #     http://hadar.amxa.ch/$ORG/$SCHOOL/tab.php?user=$USER&type=$TYPE&class=$CLASS
 
 #will probably change soon
-case $ORG in
-  anwil)
-      LIST="M_PROP M_GAMES"
+
+if test -e /etc/webmenu/env.sh
+   . /etc/webmenu/menuenv.sh
+else
+  case $ORG in
+    anwil)
+      LIST="M_PROP M_GAMES M_LESEWERKSTATT"
       FIRST=""
       ;;
-  basel)
+    basel)
       LIST="M_PROP M_WEBMENU_EDITOR M_GAMES M_LESEWERKSTATT"
       FIRST="2014A 2014B 2015A 2015B"
       ;;
-  ksnuwi)
+    ksnuwi)
       LIST="M_PROP M_GAMES"
       FIRST=""
       ;;
-  laeufelfingen)
+    laeufelfingen)
       LIST="M_PROP M_GAMES"
       FIRST=""
       ;;
-  *)
+    *)
       ;;
-esac
+  esac
 
-
-if test "$TYPE" = "student"; then
-   for F in $FIRST; do
+  if test "$USERTYPE" = "student"; then
+    for F in $FIRST; do
       if echo $GROUPS|grep -q $F; then
          export M_FIRST=1
       fi
-   done
+    done
+  fi
+
+  for L in $LIST; do
+    export $L=1
+  done
+
 fi
-
-if test -e $HOME/.config/webmenu/menu.json; then
-  export M_CUSTOM=1
-fi
-
-for L in $LIST; do
-  export $L=1
-done
-
 
---
+++ amxa-webmenu-extra_1.0-1ubuntu57.dsc
@@ -0,0 +1,17 @@
+Format: 1.0
+Source: amxa-webmenu-extra
+Binary: amxa-webmenu-extra
+Architecture: all
+Version: 1.0-1ubuntu57
+Maintainer: Basil Stotz <stotz@amxa.ch>
+Homepage: <insert the upstream URL, if relevant>
+Standards-Version: 3.9.2
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ amxa-webmenu-extra deb unknown extra
+Checksums-Sha1: 
+ ff249d931fb02a39ee6f2121d2b1fbcb83042d6e 27900 amxa-webmenu-extra_1.0-1ubuntu57.tar.gz
+Checksums-Sha256: 
+ 531d6aba3b38ae68cc47d5a5464eec37134b92c3457c2c6be42d6ad43deaa5a5 27900 amxa-webmenu-extra_1.0-1ubuntu57.tar.gz
+Files: 
+ 690477ab0731bfc2bb1759cfa5a4051f 27900 amxa-webmenu-extra_1.0-1ubuntu57.tar.gz
